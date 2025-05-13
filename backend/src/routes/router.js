3bca6a0f8eb0dff31b74eca226af23360592be01
AKASH-GPT-1678 <akashgpt5974@gmail.com>
2025-05-14 02:56:53 +0530

    Delete backend directory

--- backend/src/routes/router.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const {registerUser} = require("../controllers/register.controller.js");
-
-
-router.post("/register" , registerUser);
-
-
-module.exports = router;
