79bf55cfd542e1c1467d856ed042e1ba5d1b4f7f
Akash Gupta <acashgupta960@gmail.com>
2025-05-14 12:58:49 +0530

    Google Cloud file

--- backend/src/configs/cloud.config.js
+++ backend/src/configs/cloud.config.js
@@ -3,8 +3,8 @@ const { Storage } = require("@google-cloud/storage");
 
 
 const storage = new Storage({
-    projectId: process.env.ProjectId,
-    keyFilename: process.env.GCP_Json_file,
+    projectId: process.env.PROJECT_ID,
+    keyFilename: process.env.GCP_FILE,
 });
 
 const bucketName = process.env.BUCKET_NAME;
