bb890e4d3a1513aaa98bf986c44e0fe3b7643f27
Akash Gupta <acashgupta960@gmail.com>
2025-05-14 13:02:25 +0530

    Json file GCP

--- backend/src/configs/twilio.config.js
+++ backend/src/configs/twilio.config.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
-const twilio = require("twilio"); // Or, for ESM: import twilio from "twilio";
+const twilio = require("twilio"); 
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN; //TWILIO_AUTH_TOKEN
+const authToken = process.env.TWILIO_AUTH_TOKEN; 
 const client = twilio(accountSid, authToken);
 const nodemailer = require("nodemailer");
 const Email = process.env.Email;
